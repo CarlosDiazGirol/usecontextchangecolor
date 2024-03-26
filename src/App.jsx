59a62ccfb933988fb316f5500a369f4beba48a6f
Carlos <carlosdiazgirol@hotmail.com>
2024-03-26 14:31:44 +0100

    ready to work

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from './themes/ThemeContext';
+import RoutesApp from './routes/RoutesApp';
+import './App.css'; // Archivo de estilos CSS
+
+const App = () => {
+  return (
+    <ThemeProvider>
+      <RoutesApp />
+    </ThemeProvider>
+  );
+};
+
+export default App;
